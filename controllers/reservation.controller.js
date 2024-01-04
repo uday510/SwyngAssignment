96186699c1cc0320259ede8310ffda71985f6456
Uday <budayteja009@gmail.com>
2024-01-04 15:21:40 +0530

    Add cors

--- controllers/reservation.controller.js
+++ controllers/reservation.controller.js
@@ -8,9 +8,15 @@ const createReservation = async (req, res) => {
       return res.status(500).json({ error: "Required fields are missing." })
     }
 
+    // console.log("Body -->", req.body);
+
     const newReservation = new Reservation({ name, email, phone, slot });
 
+    // console.log("Success --> ", newReservation);
+
     const savedReservation = await newReservation.save();
+
+    // console.log("Success --> ", savedReservation);
 
     res.status(201).json(savedReservation);
   } catch (error) {
@@ -18,7 +24,6 @@ const createReservation = async (req, res) => {
     res.status(500).json({ error: 'An error occurred while creating the reservation' });
   }
 };
-
 
 module.exports = {
   createReservation
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const dbConfig = require("./configs/db.config");
 const app = express();
 
+// Logs time for every request
+function requestTime(req, res, next) {
+  process.stdout.write(
+    `Request-Date: ${new Date().toString()} ${req.method} ${req.url} \n`
+  );
+  next();
+}
+
+app.use(requestTime); // logs request time
 console.clear();
+app.use(cors({
+  origin: 'https://slot-booking-beryl.vercel.app/', // Replace with your frontend URL
+  methods: ['GET', 'POST'], // Specify the allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed headers
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
